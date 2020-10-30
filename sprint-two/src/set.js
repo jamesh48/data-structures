6eb0a08269657cb939ff5f3abe0d27942a6f0272
Carolyn Tran <carolyntran953@gmail.com>
2020-10-29 22:27:17 -0500

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -20,7 +20,7 @@ setPrototype.remove = function(item) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
- add
- contains
- remove
+ add O(1)
+ contains O(n)
+ remove O(n)
  */
