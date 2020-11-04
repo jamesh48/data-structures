7bc168f2fb24da97d3d24bf55a9b38991f4b2cee
James Hrivnak <jameshrivnak4@gmail.com>
2020-11-04 13:34:34 -0700

    Refactored so that bucketArrays are created on first insertion instead of just when a collision happens

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,60 +3,31 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
-// HashTable.prototype.insert = function(k, v) {
-//   var index = getIndexBelowMaxForKey(k, this._limit);
-
-//   if (this._storage[index] !== undefined) {
-//     alert('test');
-//   }
-//   this._storage.set(index, v);
-// };
-
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   //Test checks for hash function collisions
-  let test = this._storage.get(index);
+  let bucketArr = this._storage.get(index);
   //If there is a collison...
-  if (test !== undefined) {
-    //First save the index of the collision location
-    let saveIndex = index;
-    //Save the Value of the existing property.
-    let saveValue = test;
-    //Start from 0, and increment index until there is not a collision.
-    index = 0;
-    while (test !== undefined) {
-      index++;
-      test = this._storage.get(index);
+  if (bucketArr !== undefined) {
+    //If the key already exists...
+    if (bucketArr.some((element) => {
+      if (element.includes(k)) {
+        return true;
+      }
+    }) === true) {
+      this._storage.set(index, v);
+      return;
     }
 
-    //This gets the right key to pass the test, but I suspect that its hard coded such that it actually gets the value 'val1'...
-    //get function: return storage[index];
-    //index = 0, then 1 after the while loop, then 0  after subtracting 1;
-    //so storage[0 (val1)(I actually want this key)] = 'val1' (I don't want this value);
-    let savedKey = this._storage.get(index - 1);
-
-    //Defines bucketArr as variable...
-    let bucketArr;
-    //If the initial test revealed that a bucket arr already exists...
-    if (Array.isArray(saveValue)) {
-      bucketArr = saveValue;
-      //Simply push the new property nto that bucket as a tuple.
-      bucketArray.push([k, v]);
-    } else {
-      //Otherwise, declare bucketArr as an arr, and push in the old property, along with the new property, everyones invited!
-      bucketArr = [];
-      bucketArr.push([savedKey, saveValue], [k, v]);
-    }
-    //Set the bucket arr at the storage array index, where the collision originally took place.
-    this._storage.set(saveIndex, bucketArr);
+    bucketArr.push([k, v]);
+    this._storage.set(index, bucketArr);
     return;
-  }
+  };
 
-
-  // [0[savedKey, savedValue], [k, v]]
-  //If there is not a collision, simply set the property normally.
-  this._storage.set(index, v);
-
+  //Otherwise...
+  let newBucketArr = [];
+  newBucketArr.push([k, v]);
+  this._storage.set(index, newBucketArr);
 };
 
 // HashTable.prototype.retrieve = function(k) {
@@ -66,14 +37,12 @@ HashTable.prototype.insert = function(k, v) {
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  let test = this._storage.get(index);
-  if (Array.isArray(test)) {
-    for (let i = 0; i < test.length; i++) {
-      if (test[i][0] === k) {
-        // alert(JSON.stringify(test));
-        // alert(JSON.stringify(test[i]));
-        // alert(JSON.stringify(test[i][0]) + ' === ' + k);
-        return test[i][1];
+
+  let bucketArr = this._storage.get(index);
+  if (bucketArr !== undefined) {
+    for (let i = 0; i < bucketArr.length; i++) {
+      if (bucketArr[i][0] === k) {
+        return bucketArr[i][1];
       }
     }
   }
@@ -85,7 +54,17 @@ HashTable.prototype.remove = function(k) {
     return 'key is undefined';
   }
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.each((item, i, collection) => delete collection[index]);
+
+  this._storage.each((item, i, collection) => {
+    if (item !== undefined) {
+      for (let j = 0; j < item.length; j ++) {
+        if (item[j].includes(k)) {
+          collection.splice(index, 1);
+          j += 2;
+        }
+      }
+    }
+  });
 };
 
 
